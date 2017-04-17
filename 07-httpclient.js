5cc2d3a55ea84ed3df7750956100638fd80c4b2d
Fábio Henrique <fabiosjc@gmail.com>
2017-04-16 21:56:38 -0300

    07 - HTTP CLIENT

---
+++ 07-httpclient.js
@@ -0,0 +1,16 @@
+/* ## HTTP CLIENT (Exercise 7 of 13)
+
+  Write a program that performs an HTTP GET request to a URL provided to you
+  as the first command-line argument. Write the String contents of each
+  "data" event from the response to a new line on the console (stdout).*/
+
+let http = require('http')
+const URL = process.argv[2]
+
+http.get(URL, (res) => {
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+        console.log(chunk)
+    });
+})
+
